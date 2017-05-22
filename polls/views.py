9933af7ba88934d62675615959141462aa3a99ce
Fahad <mfahadsiddiqui1@hotmail.com>
2017-05-22 17:29:23 +0500

    loader import added and to database commits updated

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from .models import Question
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render, loader
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from .models import Choice, Question
 
 def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5]
@@ -30,4 +30,20 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
